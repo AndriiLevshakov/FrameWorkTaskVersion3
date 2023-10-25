51dc69cdf325d236384a17cee54953dff6118893
Andrey Levshakov <andreylevshakov@gmail.com>
2023-10-25 18:32:20 +0300

    Add project files.

---
+++ main/FrameWorkTaskVersion3/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace FrameWorkTaskVersion3
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
